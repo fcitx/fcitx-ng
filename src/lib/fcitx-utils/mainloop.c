03d961e12450e7140c89fce1897e3c09eb8a9668
Weng Xuetian <wengxt@gmail.com>
2013-10-20 15:54:06 -0400

    mainloop seems done

--- src/lib/fcitx-utils/mainloop.c
+++ src/lib/fcitx-utils/mainloop.c
@@ -6,6 +6,7 @@
 #include <errno.h>
 #include <sys/time.h>
 #include <time.h>
+#include <stdio.h>
 #include "utils.h"
 #include "types.h"
 #include "handler-table.h"
@@ -16,6 +17,12 @@
 static UT_icd pollfd_icd = {
     sizeof(struct pollfd), NULL, NULL, NULL
 };
+
+typedef struct _FcitxPollKey
+{
+    int fd;
+    int flag;
+} FcitxPollKey;
 
 struct _FcitxIOEvent
 {
@@ -55,6 +62,20 @@ struct _FcitxMainLoop
 
 static void fcitx_mainloop_wakeup(FcitxMainLoop* mainloop);
 static boolean get_time(int64_t* ms);
+static int fcitx_io_event_flag_to_libc_flag(int flag)
+{
+    return ((flag & FIOEF_IN) ? POLLIN : 0)
+         | ((flag & FIOEF_OUT) ? POLLOUT : 0)
+         | ((flag & FIOEF_ERR) ? POLLERR : 0)
+         | ((flag & FIOEF_HUP) ? POLLHUP : 0);
+}
+static int fcitx_io_event_flag_from_libc_flag(int flag)
+{
+    return ((flag & POLLIN) ? FIOEF_IN : 0)
+         | ((flag & POLLOUT) ? FIOEF_OUT : 0)
+         | ((flag & POLLERR) ? FIOEF_ERR : 0)
+         | ((flag & POLLHUP) ? FIOEF_HUP : 0);
+}
 
 void fd_rebuild_notify(void *data, const void* key, size_t len, void *owner)
 {
@@ -174,7 +195,34 @@ void fcitx_mainloop_dispatch_io(FcitxMainLoop* mainloop)
         return;
     }
 
+    int n = mainloop->pollRet;
+    for (size_t i = 0; i < utarray_len(&mainloop->pollfds); i++) {
+        if (mainloop->quit) {
+            break;
+        }
+        if (n <= 0) {
+            break;
+        }
 
+        struct pollfd* pfd = (struct pollfd*) utarray_eltptr(&mainloop->pollfds, i);
+        if (pfd->revents) {
+            n --;
+
+            FcitxPollKey key;
+            memset(&key, 0, sizeof(FcitxPollKey));
+            key.fd = pfd->fd;
+            key.flag = pfd->events;
+            for (FcitxIOEvent* event = fcitx_handler_table_first(mainloop->ioHandler, sizeof(FcitxPollKey), &key);
+                event;
+                event = fcitx_handler_table_next(mainloop->ioHandler, event)) {
+                event->callback(event, event->fd, fcitx_io_event_flag_from_libc_flag(pfd->revents), event->userdata);
+                if (mainloop->quit) {
+                    break;
+                }
+            }
+            pfd->revents = 0;
+        }
+    }
 }
 
 boolean get_time(int64_t* ms)
@@ -215,14 +263,14 @@ boolean get_time(int64_t* ms)
     return r == 0;
 }
 
-void foreach_key(FcitxHandlerTable* table, FcitxHandlerKey* key, void* data)
+void foreach_poll_key(FcitxHandlerTable* table, FcitxHandlerKey* key, void* data)
 {
     FcitxMainLoop* mainloop = data;
     utarray_extend_back(&mainloop->pollfds);
     struct pollfd* pfd = (struct pollfd*) utarray_back(&mainloop->pollfds);
-    int* fd = fcitx_handler_key_get_data(table, key);
-    pfd->fd = *fd;
-    pfd->events = POLLIN | POLLOUT | POLLERR | POLLHUP;
+    FcitxPollKey* pollkey = fcitx_handler_key_get_data(table, key);
+    pfd->fd = pollkey->fd;
+    pfd->events = pollkey->flag;
     pfd->revents = 0;
 }
 
@@ -234,6 +282,10 @@ int fcitx_mainloop_prepare(FcitxMainLoop* mainloop)
     int64_t now = 0;
     get_time(&now);
     mainloop->nextTimeout = -1;
+
+    char wakeUpBuf[16];
+    while (read(mainloop->wakeupPipe[0], wakeUpBuf, sizeof(wakeUpBuf)) == sizeof(wakeUpBuf));
+
     fcitx_list_entry_foreach(event, FcitxTimeoutEvent, &mainloop->timeoutHandler, list) {
         if (event->time <= now) {
             mainloop->nextTimeout = 0;
@@ -257,7 +309,7 @@ int fcitx_mainloop_prepare(FcitxMainLoop* mainloop)
         fd->fd = mainloop->wakeupPipe[0];
         fd->events = POLLIN;
         fd->revents = 0;
-        fcitx_handler_table_foreach_key(mainloop->ioHandler, foreach_key, mainloop);
+        fcitx_handler_table_foreach_key(mainloop->ioHandler, foreach_poll_key, mainloop);
     }
     return 0;
 }
@@ -278,17 +330,16 @@ int fcitx_mainloop_iterate(FcitxMainLoop* mainloop)
 {
     int r = 0;
     do {
-        if ((r = fcitx_mainloop_prepare(mainloop))) {
+        if ((r = fcitx_mainloop_prepare(mainloop)) < 0) {
         }
 
-        if ((r = fcitx_mainloop_poll(mainloop))) {
+        if ((r = fcitx_mainloop_poll(mainloop)) < 0) {
             break;
         }
 
-        if ((r = fcitx_mainloop_dispatch(mainloop))) {
+        if ((r = fcitx_mainloop_dispatch(mainloop)) < 0) {
             break;
         }
-        return r;
     } while (0);
 
     return r;
@@ -297,6 +348,7 @@ int fcitx_mainloop_iterate(FcitxMainLoop* mainloop)
 FCITX_EXPORT_API
 int fcitx_mainloop_run(FcitxMainLoop* mainloop)
 {
+    mainloop->quit = false;
     int r;
     while ((r = fcitx_mainloop_iterate(mainloop)) >= 0) {
     }
@@ -310,19 +362,25 @@ void fcitx_mainloop_quit(FcitxMainLoop* mainloop)
 }
 
 FCITX_EXPORT_API
-FcitxIOEvent* fcitx_mainloop_register_io_event(FcitxMainLoop* mainloop, int fd, FcitxIOEventCallback callback, FcitxDestroyNotify freeFunc, void* userdata)
+FcitxIOEvent* fcitx_mainloop_register_io_event(FcitxMainLoop* mainloop, int fd, int flag, FcitxIOEventCallback callback, FcitxDestroyNotify freeFunc, void* userdata)
 {
-    FcitxIOEvent* event = fcitx_utils_new(FcitxIOEvent);
-    event->fd = fd;
-    event->destroyNotify = freeFunc;
-    event->userdata = userdata;
-    event->id = fcitx_handler_table_append(mainloop->ioHandler, sizeof(int), &fd, event);
-    event->callback = callback;
-    mainloop->rebuildPollfds = true;
+    FcitxIOEvent event, *pevent;
+    event.fd = fd;
+    event.destroyNotify = freeFunc;
+    event.userdata = userdata;
+    event.callback = callback;
+
+    FcitxPollKey key;
+    memset(&key, 0, sizeof(FcitxPollKey));
+    key.fd = fd;
+    key.flag = fcitx_io_event_flag_to_libc_flag(flag);
+    int id = fcitx_handler_table_append(mainloop->ioHandler, sizeof(FcitxPollKey), &key, &event);
+    pevent = fcitx_handler_table_get_by_id(mainloop->ioHandler, id);
+    pevent->id = id;
 
     fcitx_mainloop_wakeup(mainloop);
 
-    return event;
+    return pevent;
 }
 
 FCITX_EXPORT_API
@@ -352,4 +410,24 @@ FCITX_EXPORT_API
 void fcitx_mainloop_remove_timeout_event(FcitxMainLoop* mainloop, FcitxTimeoutEvent* event)
 {
     fcitx_list_remove(&event->list);
+    if (event->destroyNotify) {
+        event->destroyNotify(event->userdata);
+    }
+    free(event);
+}
+
+FCITX_EXPORT_API
+void fcitx_mainloop_free(FcitxMainLoop* mainloop)
+{
+    fcitx_handler_table_free(mainloop->ioHandler);
+
+    fcitx_list_entry_foreach_safe(event, FcitxTimeoutEvent, &mainloop->timeoutHandler, list) {
+        fcitx_mainloop_remove_timeout_event(mainloop, event);
+    }
+
+    close(mainloop->wakeupPipe[0]);
+    close(mainloop->wakeupPipe[1]);
+    utarray_done(&mainloop->pollfds);
+
+    free(mainloop);
 }
--- test/testmainloop.c
+++ test/testmainloop.c
@@ -1,17 +1,47 @@
 #include <fcitx-utils/mainloop.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <assert.h>
+#include <stdio.h>
 
-void callback(FcitxTimeoutEvent* event, void* data)
+int p[2];
+void timeout_callback(FcitxTimeoutEvent* event, void* data)
+{
+    char c = 0;
+    assert(write(p[1], &c, sizeof(char)) == 1);
+}
+
+void timeout_callback2(FcitxTimeoutEvent* event, void* data)
 {
     FcitxMainLoop* mainloop = data;
     fcitx_mainloop_quit(mainloop);
 }
 
+
+void io_callback(FcitxIOEvent* event, int fd, int revents, void* data)
+{
+    static int counter = 0;
+    counter ++;
+
+    assert(counter == 1);
+    FcitxMainLoop* mainloop = data;
+    char c[2];
+    assert(read(p[0], c, sizeof(c)) == 1);
+
+    fcitx_mainloop_register_timeout_event(mainloop, 100, false, timeout_callback2, NULL, mainloop);
+}
+
 int main()
 {
     FcitxMainLoop* mainloop = fcitx_mainloop_new();
-    fcitx_mainloop_register_timeout_event(mainloop, 1000, false, callback, NULL, mainloop);
+    int r = pipe2(p, O_NONBLOCK);
+    assert(r == 0);
+    fcitx_mainloop_register_timeout_event(mainloop, 0, false, timeout_callback, NULL, mainloop);
+    fcitx_mainloop_register_io_event(mainloop, p[0], FIOEF_IN, io_callback, NULL, mainloop);
     fcitx_mainloop_run(mainloop);
+
+    fcitx_mainloop_free(mainloop);
 
     return 0;
 }
